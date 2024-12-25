c33059b6e8297d4f92bd94a6cb67503515afad48
yadier <yadierg7@gmail.com>
2024-12-25 18:28:54 -0400

    updated the func to interact with the db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/Yadier01/rest-api/db"
+)
 
 type Event struct {
-	ID          int       `binding:"required"`
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,12 +17,43 @@ type Event struct {
 
 var events []Event = []Event{}
 
-func (e Event) Save() {
-	//later add it to a db
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `INSERT INTO events
+  (name, description, location, dateTime, user_id)
+  VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-	return events
+	var events []Event
+
+	for rows.Next() {
+		var e Event
+		err := rows.Scan(&e.ID, &e.Name, &e.Description, &e.Location, &e.DateTime, &e.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, e)
+	}
+	return events, nil
 }
