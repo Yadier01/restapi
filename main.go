8ca66ce55faf7bf1aeba3191759990464bd905a0
yadier <yadierg7@gmail.com>
2024-12-25 18:29:12 -0400

    added init.DB to main func

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/Yadier01/rest-api/db"
 	"github.com/Yadier01/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
@@ -10,13 +11,17 @@ import (
 func main() {
 	srv := gin.Default()
 
+	db.InitDB()
 	srv.GET("/events", getEvents)
 	srv.POST("/events", createEvent)
 	srv.Run(":8080")
 }
 
 func getEvents(c *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not get events"})
+	}
 	c.JSON(http.StatusOK, events)
 }
 func createEvent(c *gin.Context) {
@@ -25,7 +30,6 @@ func createEvent(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "could not bind json"})
 		return
 	}
-	event.ID = 1
 	event.UserID = 1
 	event.Save()
 	c.JSON(http.StatusCreated, gin.H{"message": "created", "event": event})
