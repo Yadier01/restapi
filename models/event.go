fd9c402d744abdb17adec9c3fc654f8426159ce0
yadier <yadierg7@gmail.com>
2024-12-25 18:07:07 -0400

    created models

---
+++ models/event.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int       `binding:"required"`
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events []Event = []Event{}
+
+func (e Event) Save() {
+	//later add it to a db
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+
+	return events
+}
